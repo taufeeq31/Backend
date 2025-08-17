debcde4b2141054b41ac292447ec918476130aeb
Muhammad Taufeeq <muhammadtaufeeq@Muhammads-MacBook-Air-2.local>
2025-08-17 23:07:40 +0530

    api learning

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error occurred: ", error);
+    });
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server is running on port : ${process.env.PORT || 4000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed: ", error);
+  });
 
 /*
 const app = express();
